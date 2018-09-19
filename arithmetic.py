964de155242cbbb3ea18a2f654450b3ef8f4efbb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-19 12:09:11 -0700

    added power and mod

--- arithmetic.py
+++ arithmetic.py
@@ -6,12 +6,10 @@ def add(num1, num2):
 
     return num1 + num2
 
-
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
     return num1 - num2
-
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
@@ -22,6 +20,7 @@ def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
     return num1 / num2
+
 def square(num1):
     """Return the square of the input."""
 
@@ -35,6 +34,9 @@ def cube(num1):
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    return num1 ** num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+
+    return num1 % num2
